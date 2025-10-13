2221a157a1bb37906e6f61c78a10393825176d36
Akinyele Ritchie <157580464+AkinyeleRitchie@users.noreply.github.com>
2025-10-13 07:46:15 -0800

    Create product.js
    created a js file and linked to product.html

---
+++ product.js
@@ -0,0 +1,11 @@
+let currentSlide = 0;
+    const slides = document.querySelectorAll('.hero-slide');
+    function showSlide(n) {
+      slides.forEach(slide => slide.classList.remove('active'));
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].classList.add('active');
+    }
+    function nextSlide() {
+      showSlide(currentSlide + 1);
+    }
+    setInterval(nextSlide, 5000);
